0297a24390af58f822baec3a2c4281c3a549af69
Chuan <timgyes123@gmail.com>
2018-04-19 04:13:32 +0000

    inital file commit

---
+++ script/footer.js
@@ -0,0 +1,36 @@
+footer ='<footer id="_footer">\
+    <div class="footer" id="footer">\
+        <div class="container">\
+            <div class="row">\
+                <div class="col-lg-4  col-md-4 col-sm-4 col-xs-6"style="text-align:center">\
+                    <h3 >關於Attractions</h3>\
+                </div>\
+                <div class="col-lg-4  col-md-4 col-sm-4 col-xs-6"style="text-align:center">\
+                    <h3> 教學指南 </h3>\
+                    <ul>\
+                        <li> <a href="#">如何操作 How to use </a> </li>\
+                        <li> <a href="#"> 如何註冊 </a> </li>\
+                        <li> <a href="#"> 常見問題 </a> </li>\
+                    </ul>\
+                </div>\
+                <div class="col-lg-4  col-md-4 col-sm-4 col-xs-6"style="text-align:center">\
+                    <h3> 會員權益 </h3>\
+                    <ul>\
+                        <li> <a href="#"> 個人隱私保密政策 </a> </li>\
+                        <li> <a href="#"> 會員相關規定 </a> </li>\
+                        <li> <a href="#"> 服務條款 </a> </li>\
+                        <li> <a href="#"> 異業合作 </a> </li>\
+                    </ul>\
+                </div>\
+                </div>\
+            </div>\
+    <div class="footer-bottom">\
+        <div class="container">\
+            <p class="pull-left"> Copyright © 2018 Attractions Team. All right reserved. </p>\
+            <div class="pull-right">\
+            </div>\
+        </div>\
+    </div>\
+</footer>';
+document.write(footer);
+
---
+++ script/header.js
@@ -0,0 +1,31 @@
+header ='<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="title_nav" style="background:#007799">\
+  <a class="navbar-brand" href="./Index.html" id="tilte_bt">Attraction</a>\
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"style="border-width:2px">\
+    <span class="navbar-toggler-icon" id="sp"></span>\
+  </button>\
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">\
+    <ul class="navbar-nav" >\
+      <li class="nav-item"style="color:black">\
+        <a class="nav-link" href="#" style="color:black">行程規劃</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#"style="color:black">相片集</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#"style="color:black">附近景點</a>\
+      </li>\
+    </ul>\
+    <ul class="navbar-nav ml-auto">\
+      <li class="nav-item"  >\
+        <a class="nav-link" href="sign.html"title="Sign"style="border:dashed;"id="_sign">登入</a>\
+      </li>\
+    </ul>\
+  </div>\
+</nav>';
+document.write(header);
+
+
+
+
+
+
